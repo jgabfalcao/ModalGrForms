6bd55a923c00da13b2ed6b2e2c7171e7f888a684
João Falcão <jgabfalcao@gmail.com>
2025-02-02 18:30:22 -0300

    Feat: Incluindo o componente de Formulario

---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-formulario',
+  imports: [],
+  templateUrl: './formulario.component.html',
+  styleUrl: './formulario.component.css'
+})
