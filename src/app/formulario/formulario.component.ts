ce05749c1e1846d90201cad221ba0dcb3de794d1
João Falcão <jgabfalcao@gmail.com>
2025-02-02 19:19:06 -0300

    Feat: Inserindo tipagem para os valores do meu Formulario e requisição para o ViaCep

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,9 +1,60 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+interface Formulario{
+  nome: string;
+  cpf: string;
+  dataNascimento: string;
+  email: string;
+  cep: string;
+  logradouro: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+}
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './formulario.component.html',
-  styleUrl: './formulario.component.css'
+  styleUrl: './formulario.component.css',
+  standalone: true
 })
+
+export class FormularioComponent{
+  form: Formulario ={
+    nome: '',
+    cpf: '',
+    dataNascimento: '',
+    email: '',
+    cep: '',
+    logradouro: '',
+    bairro: '',
+    cidade: '',
+    estado: ''
+  };
+
+  constructor(private http: HttpClient){}
+
+  enviarFormulario(): void{
+    console.log('Dados do formulário preenchido: ', this.form);
+    alert('Formulário enviado com sucesso!');
+  }
+
+  buscarCep(): void{
+    if(this.form.cep.length === 8){
+      this.http.get<any>(`https://viacep.com.br/ws/${this.form.cep}/json/`).subscribe(dados => {
+        if(!dados.erro){
+          this.form.logradouro = dados.logradouro;
+          this.form.bairro = dados.bairro;
+          this.form.cidade = dados.localidade;
+          this.form.estado = dados.uf;
+        }
+      })
+    } else{
+      alert('CEP inválido');
+    }
+  }  
+
+}
