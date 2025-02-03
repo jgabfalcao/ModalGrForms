6a523cf53dd41053075173158446f02e2842a433
João Falcão <jgabfalcao@gmail.com>
2025-02-02 22:50:45 -0300

    Feat: Inserindo validações no CPF e no Nome

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { cpf as cpfValidator } from 'cpf-cnpj-validator';
+
 
 interface Formulario{
   nome: string;
@@ -42,19 +44,44 @@ export class FormularioComponent{
     alert('Formulário enviado com sucesso!');
   }
 
-  buscarCep(): void{
-    if(this.form.cep.length === 8){
-      this.http.get<any>(`https://viacep.com.br/ws/${this.form.cep}/json/`).subscribe(dados => {
-        if(!dados.erro){
+  formatarCpf(): void {
+    let cpf = this.form.cpf.replace(/\D/g, '');
+    if (cpf.length <= 11) {
+      cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
+      cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
+      cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+    }
+    this.form.cpf = cpf;
+  }
+
+  validarCpf(): void {
+    const cpf = this.form.cpf.replace(/\D/g, '');
+    if (cpf.length === 11 ){
+      if (!cpfValidator.isValid(cpf)) {
+        alert('CPF inválido');
+        this.form.cpf = '';
+      }
+    } 
+    
+  }
+
+  buscarCep(): void {
+    const cep = this.form.cep.replace(/\D/g, '');
+  
+    if (this.form.cep.length === 8 ) {
+      this.http.get<any>(`https://viacep.com.br/ws/${cep}/json/`).subscribe(dados => {
+        if (!dados.erro) {
           this.form.logradouro = dados.logradouro;
           this.form.bairro = dados.bairro;
           this.form.cidade = dados.localidade;
           this.form.estado = dados.uf;
+        } else {
+          alert('CEP não encontrado');
         }
-      })
-    } else{
-      alert('CEP inválido');
+      }, error => {
+        alert('Erro ao buscar CEP. Tente novamente. Digite apenas números');
+      });
     }
-  }  
+  }
 
 }
